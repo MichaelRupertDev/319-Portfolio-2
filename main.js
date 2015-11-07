dd05884e70a9298fed84b35de57c198fa7c06fb8
Michael Rupert <mbrupert@iastate.edu>
2015-11-07 13:55:48 -0600

    Added work for getting player name and making new Players

---
+++ main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+	/**
+		Wheel Logic
+	*/
+	var current_angle = 0;
+	var desired_angle = 0;
+	var values = ["Bankrupt", "$500", "$175", "$300", "$200", "$75", "$125", "$100", "$25", "$200" 
+					, "Free Spin", "$100", "$200", "$50", "$450", "Lose A Turn", "$100", "$275", "$75", "$150"
+					, "Buy A Vowel", "$100", "$250", "$50"];
+	// spins the wheel
+    spinWheel = function(){
+		desired_angle = Math.floor((Math.random() * 900) + 1);
+		while(Math.abs(desired_angle - current_angle) < 360){
+			desired_angle = Math.floor((Math.random() * 1800) + 1);
+		}
+		
+		$("#wheel").rotate(desired_angle);
+		current_angle = desired_angle;
+		var adjusted = current_angle;
+		while(adjusted > 360){
+			adjusted = adjusted % 360;
+		}
+		//var adj = Math.ceil((360 - adjusted)/ 360 * 24);
+		var adj = (360 - adjusted)/ 360 * 24;
+		var rem = adj;
+		while(rem > 1){
+			rem --
+		};
+		if(rem > 0.5){
+			adj = Math.ceil(adj);
+		}
+		else{
+			adj = Math.floor(adj);
+		}
+		if(values[adj] == "Bankrupt") game.state == "ENDTURN";
+		else game.state = "CHOOSE";
+    };
+	
+	// helper function for rotate 
+	jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+	
+	/**  End of Wheel Logic */
+
+	};
+
+	var name = prompt("Please enter your name");
+	player1 = new Player(name);
+	game = new WOF();
+	game.players = [player1, new Player('Player 2'), new Player('Player 3')];
+	game.drawPuzzle("this is a test");
+	var i = 0;
+	while(game.round < 30){
+		game.players[i].doTurn();
+		if(i == 2){
+			i = 0;
+			game.round++;
+		}
+		else i++;
+	}
+});
---
+++ player.js
@@ -0,0 +1,49 @@
+Player = function(name) {
+	this.name = name;
+	this.score = 0;
+	this.spin_value = 0;
+
+	this.doTurn = function(){
+		if(game.state == "SPIN") {
+			spinWheel();
+			this.doTurn(game.state);
+		}
+		if(game.state == "CHOOSE") {
+			var letter = prompt("Choose a letter: ");
+			this.selectLetter(letter);
+		}
+		if(game.state == "ENDTURN") {
+			return;
+		}
+		if(game.state == "SOLVED"){
+			alert("SOLVED");
+		}
+	}
+
+	this.solve = function() {
+		var answer = prompt("Would you like to solve? Type answer or N for no.");
+		if(answer.toLowerCase() == "n"){
+			return;
+		}
+		if(answer.toLowerCase() == game.phrase){
+			game.state == "SOLVED";
+		}
+	}
+
+	this.selectLetter = function(letter) {
+    var matches = $("[id=" + letter.toLowerCase() + "]");
+    matches.css("color", "black");
+    if(matches.length != 0){
+        this.score += matches.length * this.spin_value;
+        matches.attr('id', 'chosen');
+        this.solve();
+        game.state = "SPIN";
+        this.doTurn(game.state);
+    }
+    else{
+    	game.state = "ENDTURN";
+    	this.doTurn(game.state);
+    }
+  }
+
+}
